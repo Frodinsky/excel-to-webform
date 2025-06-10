b71f8bac6b580871f351a4cc625e077f18929b76
Frodinsky <rodolara97@gmail.com>
2025-06-10 16:17:09 -0700

    Validacion y reporte funcionando al 100%

--- app/validation/validation_pseudomain.py
+++ app/validation/validation_pseudomain.py
@@ -1,17 +1,19 @@
 import os
 from validator import ValidadorExcel
-from report_generator import ReporteErroresExcel
+from multi_error_sheets import ReporteErroresMultiplesHojas
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_PATH = os.path.join(BASE_DIR, "config/settings.yaml")
 
 if __name__ == "__main__":
     validador = ValidadorExcel(CONFIG_PATH)
-    errores = validador.validar()
+    reporte_por_hoja = validador.validar()
 
-    if errores:
-        print(f"❌ Se encontraron errores en {len(errores)} fila(s).")
-        reporte = ReporteErroresExcel(validador.df, errores, CONFIG_PATH)
-        reporte.exportar()
-    else:
+    if not reporte_por_hoja:
         print("✅ Todos los datos son válidos.")
+    else:
+        total_errores = sum(len(d["errores"]) for d in reporte_por_hoja.values())
+        print(f"❌ Se encontraron errores en {total_errores} fila(s) con errores.")
+
+        reporte = ReporteErroresMultiplesHojas(reporte_por_hoja, ruta_config=CONFIG_PATH)
+        reporte.exportar()
