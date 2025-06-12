30444584c789b7f5a662adf6a500c420e2c92e47
Frodinsky <rodolara97@gmail.com>
2025-06-11 20:54:59 -0700

    arreglando boton de automation

--- app/automation/form_filler.py
+++ app/automation/form_filler.py
@@ -2,12 +2,13 @@ import pandas as pd
 from app.automation.form_controller import FormularioController
 
 class FormFiller:
-    def __init__(self, excel_path: str):
-        self.excel_path = excel_path
+    def __init__(self, file_path: str):
+        self.file_path = file_path
 
     def ejecutar(self):
+        print(f"Usando archivo: {self.file_path}")
         # Lee el Excel, por ejemplo con pandas
-        df = pd.read_excel(self.excel_path)
+        df = pd.read_excel(self.file_path)
 
         # Asumimos que tomas la primera fila del Excel
         datos = df.iloc[0].to_dict()
--- app/ui/archivo_validador_handler.py
+++ app/ui/archivo_validador_handler.py
@@ -3,16 +3,29 @@ import yaml
 import flet as ft
 from app.validation.validator import ValidadorExcel
 from app.reports.multi_error_sheets import ReporteErroresMultiplesHojas
-
+from app.automation.form_filler import FormFiller
 
 class ArchivoValidadorHandler:
     def __init__(self, page: ft.Page):
         self.page = page
         self.result_text = ft.Text(visible=False, size=16)
+        self.file_path = None
+        self.automation_button = ft.ElevatedButton(
+            text="Llenar Formulario",
+            visible=False,
+            on_click=self.ejecutar_llenado
+        )
         self.container = ft.Container(
-            content=self.result_text,
-            padding=ft.padding.only(top=20),
-            bgcolor=ft.Colors.with_opacity(0.07, ft.Colors.RED_200),
+            content=ft.Column(
+                controls=[
+                    self.result_text,
+                    self.automation_button
+                ],
+                spacing=10,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            ),
+            padding=10,
+            bgcolor=ft.Colors.with_opacity(0.17, ft.Colors.RED_200),
             border_radius=10,
             visible=False,
         )
@@ -21,6 +34,7 @@ class ArchivoValidadorHandler:
         return self.container
 
     def validate_file(self, file_path: str):
+        self.file_path = file_path
         # Ruta al settings.yaml
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         config_path = os.path.join(BASE_DIR, "config/settings.yaml")
@@ -37,13 +51,27 @@ class ArchivoValidadorHandler:
             self.result_text.value = "✅ Archivo válido"
             self.result_text.color = "green"
             self.container.bgcolor = ft.Colors.with_opacity(0.05, ft.Colors.GREEN_200)
+            self.automation_button.visible = True
         else:
             reporte = ReporteErroresMultiplesHojas(errores, ruta_config=config_path)
             ruta_reporte = reporte.exportar()
             self.result_text.value = f"❌ Se creo un archivo {ruta_reporte}"
             self.result_text.color = "red"
             self.container.bgcolor = ft.Colors.with_opacity(0.07, ft.Colors.RED_200)
+            self.automation_button.visible = False
 
         self.result_text.visible = True
         self.container.visible = True
         self.page.update()
+
+    def ejecutar_llenado(self, e):
+        if self.file_path:
+            print("Ejecutando llenado con:", self.file_path)
+            try:
+                llenador = FormFiller(self.file_path)
+                llenador.ejecutar()
+                self.page.snack_bar = ft.SnackBar(ft.Text("✅ Formulario enviado con éxito"))
+            except Exception as ex:
+                self.page.snack_bar = ft.SnackBar(ft.Text(f"❌ Error al llenar formulario: {str(ex)}"))
+            self.page.snack_bar.open = True
+            self.page.update()
