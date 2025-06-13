64af6e2633e476215b772101f6a02bae3e9a798c
Frodinsky <rodolara97@gmail.com>
2025-06-12 17:18:43 -0700

    Eliminar archivos requisitos.txt y app/config/utils.py

--- app/config/utils.py
+++
@@ -1,8 +0,0 @@
-# utils.py
-import os
-
-#ruta absoluta del settings.yaml
-def get_config_path(filename="settings.yaml", subdir="config"):
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_dir, subdir, filename)
-
